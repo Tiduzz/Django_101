0f514acae2f0005c8b6c688259ce8f4bdad538c6
Tiduzz - Doug <tiduzzscarlet@gmail.com>
2021-08-07 18:23:27 -0300

    Created /agenda page, refactored display on timestamps, added a pretty bad watermark

--- Agenda/core/views.py
+++ Agenda/core/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.http import response
+from django.shortcuts import redirect, render
+from core.models import Evento
 # Create your views here.
+
+def lista_eventos(request):
+    evento = Evento.objects.filter()
+    dados = {'eventos' : evento}
+    return render(request, 'agenda.html', dados)
+
+def index(request):
+    return redirect('/agenda/')
