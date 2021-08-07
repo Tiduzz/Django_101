e148e31d125365aa2319b9537d3ddc4a75d2c297
Tiduzz - Doug <tiduzzscarlet@gmail.com>
2021-08-07 15:20:50 -0300

    added new features: filter and users

--- Agenda/core/admin.py
+++ Agenda/core/admin.py
@@ -1,3 +1,10 @@
+from core.models import Evento
 from django.contrib import admin
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'date_evento', 'date_creation')
+    list_filter = ('titulo',)
+
+admin.site.register(Evento, EventoAdmin)
