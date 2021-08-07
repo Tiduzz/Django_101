ef0ab67511696206eadde2f28ca6603274d07b28
Tiduzz - Doug <tiduzzscarlet@gmail.com>
2021-08-06 22:53:17 -0300

    added new url with the sum def

--- Welcome_to_the_Django/core/views.py
+++ Welcome_to_the_Django/core/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome):
     return HttpResponse("Hello {}".format(nome))
+
+def soma(request, val1, val2):
+    return HttpResponse("O valor da soma é {}".format(val1+val2))
