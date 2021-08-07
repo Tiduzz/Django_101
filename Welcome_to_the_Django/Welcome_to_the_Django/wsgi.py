6ff562575322cc7336e6597569712a2ebbc32426
Tiduzz - Doug <tiduzzscarlet@gmail.com>
2021-08-06 22:41:33 -0300

    Initial app commit. Returns Hello World

---
+++ Welcome_to_the_Django/Welcome_to_the_Django/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Welcome_to_the_Django project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Welcome_to_the_Django.settings')
+
+application = get_wsgi_application()
